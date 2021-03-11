9b01adfb60f0a6b176f72ce25eaa11fc0e1c2786
Amelia Chin <amelia.chin@live.ca>
2021-03-11 12:37:44 -0700

    Added enrolled courses data set and more

--- CMPT391Project/CMPT391Project/Course_Registration.Designer.cs
+++ CMPT391Project/CMPT391Project/Course_Registration.Designer.cs
@@ -28,18 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.Back_Button = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.Cart_Button = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.yearComboBox = new System.Windows.Forms.ComboBox();
+            this.sectionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sectionYearDataSet = new CMPT391Project.SectionYearDataSet();
+            this.termComboBox = new System.Windows.Forms.ComboBox();
+            this.sectionBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.sectionTermDataSet = new CMPT391Project.SectionTermDataSet();
+            this.sectionBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.collegeDataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.collegeDataSet1 = new CMPT391Project.CollegeDataSet1();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.departmentComboBox = new System.Windows.Forms.ComboBox();
+            this.departmentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.collegeDataSet1BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.sectionTableAdapter = new CMPT391Project.SectionYearDataSetTableAdapters.sectionTableAdapter();
+            this.sectionTableAdapter1 = new CMPT391Project.CollegeDataSet1TableAdapters.sectionTableAdapter();
+            this.departmentTableAdapter = new CMPT391Project.CollegeDataSet1TableAdapters.departmentTableAdapter();
+            this.sectionTableAdapter2 = new CMPT391Project.SectionTermDataSetTableAdapters.sectionTableAdapter();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionYearDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionTermDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet1BindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -83,22 +106,66 @@
             this.Cart_Button.TabIndex = 6;
             this.Cart_Button.Text = "Cart";
             this.Cart_Button.UseVisualStyleBackColor = true;
+            this.Cart_Button.Click += new System.EventHandler(this.Cart_Button_Click);
             // 
-            // comboBox1
+            // yearComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(104, 176);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 7;
+            this.yearComboBox.DataSource = this.sectionBindingSource;
+            this.yearComboBox.DisplayMember = "year";
+            this.yearComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.yearComboBox.FormattingEnabled = true;
+            this.yearComboBox.Location = new System.Drawing.Point(104, 176);
+            this.yearComboBox.Name = "yearComboBox";
+            this.yearComboBox.Size = new System.Drawing.Size(121, 28);
+            this.yearComboBox.TabIndex = 7;
+            this.yearComboBox.ValueMember = "year";
             // 
-            // comboBox2
+            // sectionBindingSource
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(344, 176);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(152, 28);
-            this.comboBox2.TabIndex = 8;
+            this.sectionBindingSource.DataMember = "section";
+            this.sectionBindingSource.DataSource = this.sectionYearDataSet;
+            // 
+            // sectionYearDataSet
+            // 
+            this.sectionYearDataSet.DataSetName = "SectionYearDataSet";
+            this.sectionYearDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // termComboBox
+            // 
+            this.termComboBox.DataSource = this.sectionBindingSource2;
+            this.termComboBox.DisplayMember = "term";
+            this.termComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.termComboBox.FormattingEnabled = true;
+            this.termComboBox.Location = new System.Drawing.Point(344, 176);
+            this.termComboBox.Name = "termComboBox";
+            this.termComboBox.Size = new System.Drawing.Size(152, 28);
+            this.termComboBox.TabIndex = 8;
+            this.termComboBox.ValueMember = "term";
+            // 
+            // sectionBindingSource2
+            // 
+            this.sectionBindingSource2.DataMember = "section";
+            this.sectionBindingSource2.DataSource = this.sectionTermDataSet;
+            // 
+            // sectionTermDataSet
+            // 
+            this.sectionTermDataSet.DataSetName = "SectionTermDataSet";
+            this.sectionTermDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sectionBindingSource1
+            // 
+            this.sectionBindingSource1.DataMember = "section";
+            this.sectionBindingSource1.DataSource = this.collegeDataSet1BindingSource;
+            // 
+            // collegeDataSet1BindingSource
+            // 
+            this.collegeDataSet1BindingSource.DataSource = this.collegeDataSet1;
+            this.collegeDataSet1BindingSource.Position = 0;
+            // 
+            // collegeDataSet1
+            // 
+            this.collegeDataSet1.DataSetName = "CollegeDataSet1";
+            this.collegeDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // label2
             // 
@@ -130,13 +197,27 @@
             this.label4.TabIndex = 12;
             this.label4.Text = "Department";
             // 
-            // comboBox3
+            // departmentComboBox
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(680, 176);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(280, 28);
-            this.comboBox3.TabIndex = 11;
+            this.departmentComboBox.DataSource = this.departmentBindingSource;
+            this.departmentComboBox.DisplayMember = "name";
+            this.departmentComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.departmentComboBox.FormattingEnabled = true;
+            this.departmentComboBox.Location = new System.Drawing.Point(680, 176);
+            this.departmentComboBox.Name = "departmentComboBox";
+            this.departmentComboBox.Size = new System.Drawing.Size(280, 28);
+            this.departmentComboBox.TabIndex = 11;
+            this.departmentComboBox.ValueMember = "name";
+            // 
+            // departmentBindingSource
+            // 
+            this.departmentBindingSource.DataMember = "department";
+            this.departmentBindingSource.DataSource = this.collegeDataSet1BindingSource1;
+            // 
+            // collegeDataSet1BindingSource1
+            // 
+            this.collegeDataSet1BindingSource1.DataSource = this.collegeDataSet1;
+            this.collegeDataSet1BindingSource1.Position = 0;
             // 
             // dataGridView1
             // 
@@ -147,6 +228,22 @@
             this.dataGridView1.Size = new System.Drawing.Size(1504, 896);
             this.dataGridView1.TabIndex = 13;
             // 
+            // sectionTableAdapter
+            // 
+            this.sectionTableAdapter.ClearBeforeFill = true;
+            // 
+            // sectionTableAdapter1
+            // 
+            this.sectionTableAdapter1.ClearBeforeFill = true;
+            // 
+            // departmentTableAdapter
+            // 
+            this.departmentTableAdapter.ClearBeforeFill = true;
+            // 
+            // sectionTableAdapter2
+            // 
+            this.sectionTableAdapter2.ClearBeforeFill = true;
+            // 
             // Course_Registration
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -154,17 +251,27 @@
             this.ClientSize = new System.Drawing.Size(1584, 1161);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.departmentComboBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.termComboBox);
+            this.Controls.Add(this.yearComboBox);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Course_Registration";
             this.Text = "Course Registration";
+            this.Load += new System.EventHandler(this.Course_Registration_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sectionBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionYearDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionTermDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sectionBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet1BindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -176,13 +283,26 @@
         private System.Windows.Forms.Button Back_Button;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button Cart_Button;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox yearComboBox;
+        private System.Windows.Forms.ComboBox termComboBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox departmentComboBox;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private SectionYearDataSet sectionYearDataSet;
+        private System.Windows.Forms.BindingSource sectionBindingSource;
+        private SectionYearDataSetTableAdapters.sectionTableAdapter sectionTableAdapter;
+        private System.Windows.Forms.BindingSource collegeDataSet1BindingSource;
+        private CollegeDataSet1 collegeDataSet1;
+        private System.Windows.Forms.BindingSource sectionBindingSource1;
+        private CollegeDataSet1TableAdapters.sectionTableAdapter sectionTableAdapter1;
+        private System.Windows.Forms.BindingSource collegeDataSet1BindingSource1;
+        private System.Windows.Forms.BindingSource departmentBindingSource;
+        private CollegeDataSet1TableAdapters.departmentTableAdapter departmentTableAdapter;
+        private SectionTermDataSet sectionTermDataSet;
+        private System.Windows.Forms.BindingSource sectionBindingSource2;
+        private SectionTermDataSetTableAdapters.sectionTableAdapter sectionTableAdapter2;
     }
 }
 
--- CMPT391Project/CMPT391Project/EnrolledCourses.Designer.cs
+++ CMPT391Project/CMPT391Project/EnrolledCourses.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.enrolledCoursesDataGridView1 = new System.Windows.Forms.DataGridView();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.creditDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.termDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departmentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkEnrolledBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.enrolledCoursesDataSet = new CMPT391Project.EnrolledCoursesDataSet();
             this.panel1 = new System.Windows.Forms.Panel();
             this.Cart_Button = new System.Windows.Forms.Button();
             this.Back_Button = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.checkEnrolledTableAdapter = new CMPT391Project.EnrolledCoursesDataSetTableAdapters.checkEnrolledTableAdapter();
+            this.fillToolStrip = new System.Windows.Forms.ToolStrip();
+            this.studentIDToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.studentIDToolStripTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.fillToolStripButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.enrolledCoursesDataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEnrolledBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enrolledCoursesDataSet)).BeginInit();
             this.panel1.SuspendLayout();
+            this.fillToolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -47,13 +64,80 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Enrolled Courses";
             // 
-            // dataGridView1
+            // enrolledCoursesDataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(32, 168);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(720, 720);
-            this.dataGridView1.TabIndex = 8;
+            this.enrolledCoursesDataGridView1.AllowUserToAddRows = false;
+            this.enrolledCoursesDataGridView1.AllowUserToDeleteRows = false;
+            this.enrolledCoursesDataGridView1.AutoGenerateColumns = false;
+            this.enrolledCoursesDataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.enrolledCoursesDataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.enrolledCoursesDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.enrolledCoursesDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.enrolledCoursesDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.titleDataGridViewTextBoxColumn,
+            this.creditDataGridViewTextBoxColumn,
+            this.termDataGridViewTextBoxColumn,
+            this.departmentDataGridViewTextBoxColumn});
+            this.enrolledCoursesDataGridView1.DataSource = this.checkEnrolledBindingSource;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.enrolledCoursesDataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.enrolledCoursesDataGridView1.Location = new System.Drawing.Point(32, 168);
+            this.enrolledCoursesDataGridView1.Name = "enrolledCoursesDataGridView1";
+            this.enrolledCoursesDataGridView1.ReadOnly = true;
+            this.enrolledCoursesDataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.enrolledCoursesDataGridView1.Size = new System.Drawing.Size(720, 720);
+            this.enrolledCoursesDataGridView1.TabIndex = 8;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.titleDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // creditDataGridViewTextBoxColumn
+            // 
+            this.creditDataGridViewTextBoxColumn.DataPropertyName = "credit";
+            this.creditDataGridViewTextBoxColumn.HeaderText = "credit";
+            this.creditDataGridViewTextBoxColumn.Name = "creditDataGridViewTextBoxColumn";
+            this.creditDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // termDataGridViewTextBoxColumn
+            // 
+            this.termDataGridViewTextBoxColumn.DataPropertyName = "term";
+            this.termDataGridViewTextBoxColumn.HeaderText = "term";
+            this.termDataGridViewTextBoxColumn.Name = "termDataGridViewTextBoxColumn";
+            this.termDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // departmentDataGridViewTextBoxColumn
+            // 
+            this.departmentDataGridViewTextBoxColumn.DataPropertyName = "department";
+            this.departmentDataGridViewTextBoxColumn.HeaderText = "department";
+            this.departmentDataGridViewTextBoxColumn.Name = "departmentDataGridViewTextBoxColumn";
+            this.departmentDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // checkEnrolledBindingSource
+            // 
+            this.checkEnrolledBindingSource.DataMember = "checkEnrolled";
+            this.checkEnrolledBindingSource.DataSource = this.enrolledCoursesDataSet;
+            // 
+            // enrolledCoursesDataSet
+            // 
+            this.enrolledCoursesDataSet.DataSetName = "EnrolledCoursesDataSet";
+            this.enrolledCoursesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // panel1
             // 
@@ -86,18 +170,61 @@
             this.Back_Button.UseVisualStyleBackColor = true;
             this.Back_Button.Click += new System.EventHandler(this.Back_Button_Click);
             // 
+            // checkEnrolledTableAdapter
+            // 
+            this.checkEnrolledTableAdapter.ClearBeforeFill = true;
+            // 
+            // fillToolStrip
+            // 
+            this.fillToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.studentIDToolStripLabel,
+            this.studentIDToolStripTextBox,
+            this.fillToolStripButton});
+            this.fillToolStrip.Location = new System.Drawing.Point(0, 56);
+            this.fillToolStrip.Name = "fillToolStrip";
+            this.fillToolStrip.Size = new System.Drawing.Size(784, 25);
+            this.fillToolStrip.TabIndex = 10;
+            this.fillToolStrip.Text = "fillToolStrip";
+            // 
+            // studentIDToolStripLabel
+            // 
+            this.studentIDToolStripLabel.Name = "studentIDToolStripLabel";
+            this.studentIDToolStripLabel.Size = new System.Drawing.Size(61, 22);
+            this.studentIDToolStripLabel.Text = "studentID:";
+            // 
+            // studentIDToolStripTextBox
+            // 
+            this.studentIDToolStripTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.studentIDToolStripTextBox.Name = "studentIDToolStripTextBox";
+            this.studentIDToolStripTextBox.ReadOnly = true;
+            this.studentIDToolStripTextBox.Size = new System.Drawing.Size(100, 25);
+            // 
+            // fillToolStripButton
+            // 
+            this.fillToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillToolStripButton.Name = "fillToolStripButton";
+            this.fillToolStripButton.Size = new System.Drawing.Size(26, 22);
+            this.fillToolStripButton.Text = "Fill";
+            this.fillToolStripButton.Click += new System.EventHandler(this.fillToolStripButton_Click);
+            // 
             // EnrolledCourses
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 911);
+            this.Controls.Add(this.fillToolStrip);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.enrolledCoursesDataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "EnrolledCourses";
             this.Text = "EnrolledCourses";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.EnrolledCourses_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.enrolledCoursesDataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEnrolledBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enrolledCoursesDataSet)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.fillToolStrip.ResumeLayout(false);
+            this.fillToolStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,9 +233,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView enrolledCoursesDataGridView1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button Cart_Button;
         private System.Windows.Forms.Button Back_Button;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn creditDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn termDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn departmentDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource checkEnrolledBindingSource;
+        private EnrolledCoursesDataSet enrolledCoursesDataSet;
+        private EnrolledCoursesDataSetTableAdapters.checkEnrolledTableAdapter checkEnrolledTableAdapter;
+        private System.Windows.Forms.ToolStrip fillToolStrip;
+        private System.Windows.Forms.ToolStripLabel studentIDToolStripLabel;
+        private System.Windows.Forms.ToolStripTextBox studentIDToolStripTextBox;
+        private System.Windows.Forms.ToolStripButton fillToolStripButton;
     }
 }
