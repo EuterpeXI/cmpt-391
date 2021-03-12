136c072f9c63e2bb891810b29b23601ce8729317
Iain Smith <iainnbsmith@gmail.com>
2021-03-11 22:55:10 -0700

    added retrieving classes that can be taken and most steps to passing to cart

---
+++ CMPT391Project/CMPT391Project/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CMPT391Project
+{
+    class SQLController
+    {
+
+        private SqlConnection sqlCon;
+        private SqlCommand command;
+        // set to local server for whoever is using
+        private static String conStr = "Server = DESKTOP-BR2712G; Database = College; Trusted_Connection = yes;";
+        public SQLController()
+        {
+
+            // on initialization make and check connection then error if it does not work
+            try
+            {
+                sqlCon = new SqlConnection(conStr);
+                command = new SqlCommand();
+                sqlCon.Open();
+                sqlCon.Close();
+            }
+            catch (Exception e)
+            {
+                //error message
+                MessageBox.Show(e.ToString(), "Error");
+
+            }
+        }
+
+        public DataSet executeFetchCommand(string commandString)
+        {
+            /*
+             * Input: commandString(string) -> query to execute.
+             * Purpose: Take a command entered and execute it retrieve the queried values in a DataSet
+             * Output: DataSet -> DataSet filled with the reulting querie's values
+             */
+            try
+            {
+                sqlCon.Open();
+                // start the command
+                command = new SqlCommand(commandString, sqlCon);
+                // move into data adapter
+                SqlDataAdapter sqlAdapt = new SqlDataAdapter(command);
+
+                //fill data set
+                DataSet data = new DataSet();
+                sqlAdapt.Fill(data);
+
+                sqlCon.Close();
+
+                return data;
+
+            }
+            catch (Exception e)
+            {
+                // error message
+                MessageBox.Show(e.ToString(), "Error");
+                return null;
+            }
+
+        }
+
+        public string getRecentQuery()
+        {
+            return command.CommandText;
+        }
+
+        public void executeSetCommand(string commandString)
+        {
+            /*
+             * Input: commandString(string) -> update or deletion to execute.
+             * Purpose: Take a command entered and execute it
+             * Output: None
+             */
+            try
+            {
+                sqlCon.Open();
+                command = new SqlCommand(commandString, sqlCon);
+                command.ExecuteNonQuery();
+
+                sqlCon.Close();
+            }
+            catch (Exception e)
+            {
+                // error message
+                MessageBox.Show(e.ToString(), "Error");
+            }
+        }
+
+    }
+}
+
